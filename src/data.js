099e0e6fcc7a5a9cb30629285a048e16f9a05a52
minh_w <minhsiungw@gmail.com>
2022-05-10 11:19:13 +0800

    add dummyData and jsconfig.json

---
+++ src/data.js
@@ -0,0 +1,254 @@
+export default [
+  {
+    name: '112',
+    regular: {
+      large: require('./assets/thumbnails/112/regular/large.jpg'),
+      medium: require('./assets/thumbnails/112/regular/medium.jpg'),
+      small: require('./assets/thumbnails/112/regular/small.jpg'),
+    },
+  },
+  {
+    name: '1998',
+    regular: {
+      large: require('./assets/thumbnails/1998/regular/large.jpg'),
+      medium: require('./assets/thumbnails/1998/regular/medium.jpg'),
+      small: require('./assets/thumbnails/1998/regular/small.jpg'),
+    },
+    trending: {
+      large: require('./assets/thumbnails/1998/trending/large.jpg'),
+      small: require('./assets/thumbnails/1998/trending/small.jpg'),
+    },
+  },
+  {
+    name: 'Asia in 24 days',
+    regular: {
+      large: require('./assets/thumbnails/asia-in-24-days/regular/large.jpg'),
+      medium: require('./assets/thumbnails/asia-in-24-days/regular/medium.jpg'),
+      small: require('./assets/thumbnails/asia-in-24-days/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Autosport the Series',
+    regular: {
+      large: require('./assets/thumbnails/autosport-the-series/regular/large.jpg'),
+      medium: require('./assets/thumbnails/autosport-the-series/regular/medium.jpg'),
+      small: require('./assets/thumbnails/autosport-the-series/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Below Echo',
+    regular: {
+      large: require('./assets/thumbnails/below-echo/regular/large.jpg'),
+      medium: require('./assets/thumbnails/below-echo/regular/medium.jpg'),
+      small: require('./assets/thumbnails/below-echo/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Beyond Earth',
+    regular: {
+      large: require('./assets/thumbnails/beyond-earth/regular/large.jpg'),
+      medium: require('./assets/thumbnails/beyond-earth/regular/medium.jpg'),
+      small: require('./assets/thumbnails/beyond-earth/regular/small.jpg'),
+    },
+    trending: {
+      large: require('./assets/thumbnails/beyond-earth/trending/large.jpg'),
+      small: require('./assets/thumbnails/beyond-earth/trending/small.jpg'),
+    },
+  },
+  {
+    name: 'Bottom Gear',
+    regular: {
+      large: require('./assets/thumbnails/bottom-gear/regular/large.jpg'),
+      medium: require('./assets/thumbnails/bottom-gear/regular/medium.jpg'),
+      small: require('./assets/thumbnails/bottom-gear/regular/small.jpg'),
+    },
+    trending: {
+      large: require('./assets/thumbnails/bottom-gear/trending/large.jpg'),
+      small: require('./assets/thumbnails/bottom-gear/trending/small.jpg'),
+    },
+  },
+  {
+    name: 'Community of Ours',
+    regular: {
+      large: require('./assets/thumbnails/community-of-ours/regular/large.jpg'),
+      medium: require('./assets/thumbnails/community-of-ours/regular/medium.jpg'),
+      small: require('./assets/thumbnails/community-of-ours/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Dark Side of the Moon',
+    regular: {
+      large: require('./assets/thumbnails/dark-side-of-the-moon/regular/large.jpg'),
+      medium: require('./assets/thumbnails/dark-side-of-the-moon/regular/medium.jpg'),
+      small: require('./assets/thumbnails/dark-side-of-the-moon/regular/small.jpg'),
+    },
+    trending: {
+      large: require('./assets/thumbnails/dark-side-of-the-moon/trending/large.jpg'),
+      small: require('./assets/thumbnails/dark-side-of-the-moon/trending/small.jpg'),
+    },
+  },
+  {
+    name: 'Darker',
+    regular: {
+      large: require('./assets/thumbnails/darker/regular/large.jpg'),
+      medium: require('./assets/thumbnails/darker/regular/medium.jpg'),
+      small: require('./assets/thumbnails/darker/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'dogs',
+    regular: {
+      large: require('./assets/thumbnails/dogs/regular/large.jpg'),
+      medium: require('./assets/thumbnails/dogs/regular/medium.jpg'),
+      small: require('./assets/thumbnails/dogs/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'During the Hunt',
+    regular: {
+      large: require('./assets/thumbnails/during-the-hunt/regular/large.jpg'),
+      medium: require('./assets/thumbnails/during-the-hunt/regular/medium.jpg'),
+      small: require('./assets/thumbnails/during-the-hunt/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Earths Untouched',
+    regular: {
+      large: require('./assets/thumbnails/earths-untouched/regular/large.jpg'),
+      medium: require('./assets/thumbnails/earths-untouched/regular/medium.jpg'),
+      small: require('./assets/thumbnails/earths-untouched/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Lone Heart',
+    regular: {
+      large: require('./assets/thumbnails/lone-heart/regular/large.jpg'),
+      medium: require('./assets/thumbnails/lone-heart/regular/medium.jpg'),
+      small: require('./assets/thumbnails/lone-heart/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Mission Saturn',
+    regular: {
+      large: require('./assets/thumbnails/mission-saturn/regular/large.jpg'),
+      medium: require('./assets/thumbnails/mission-saturn/regular/medium.jpg'),
+      small: require('./assets/thumbnails/mission-saturn/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'No Land Beyond',
+    regular: {
+      large: require('./assets/thumbnails/no-land-beyond/regular/large.jpg'),
+      medium: require('./assets/thumbnails/no-land-beyond/regular/medium.jpg'),
+      small: require('./assets/thumbnails/no-land-beyond/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Off the Track',
+    regular: {
+      large: require('./assets/thumbnails/off-the-track/regular/large.jpg'),
+      medium: require('./assets/thumbnails/off-the-track/regular/medium.jpg'),
+      small: require('./assets/thumbnails/off-the-track/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Production Line',
+    regular: {
+      large: require('./assets/thumbnails/production-line/regular/large.jpg'),
+      medium: require('./assets/thumbnails/production-line/regular/medium.jpg'),
+      small: require('./assets/thumbnails/production-line/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'RelentLess',
+    regular: {
+      large: require('./assets/thumbnails/relentless/regular/large.jpg'),
+      medium: require('./assets/thumbnails/relentless/regular/medium.jpg'),
+      small: require('./assets/thumbnails/relentless/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Same Answer 2',
+    regular: {
+      large: require('./assets/thumbnails/same-answer-2/regular/large.jpg'),
+      medium: require('./assets/thumbnails/same-answer-2/regular/medium.jpg'),
+      small: require('./assets/thumbnails/same-answer-2/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'The Diary',
+    regular: {
+      large: require('./assets/thumbnails/the-diary/regular/large.jpg'),
+      medium: require('./assets/thumbnails/the-diary/regular/medium.jpg'),
+      small: require('./assets/thumbnails/the-diary/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'The Great Lands',
+    regular: {
+      large: require('./assets/thumbnails/the-great-lands/regular/large.jpg'),
+      medium: require('./assets/thumbnails/the-great-lands/regular/medium.jpg'),
+      small: require('./assets/thumbnails/the-great-lands/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'The Heiress',
+    regular: {
+      large: require('./assets/thumbnails/the-heiress/regular/large.jpg'),
+      medium: require('./assets/thumbnails/the-heiress/regular/medium.jpg'),
+      small: require('./assets/thumbnails/the-heiress/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'The Rockies',
+    regular: {
+      large: require('./assets/thumbnails/the-rockies/regular/large.jpg'),
+      medium: require('./assets/thumbnails/the-rockies/regular/medium.jpg'),
+      small: require('./assets/thumbnails/the-rockies/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'The Tasty Tour',
+    regular: {
+      large: require('./assets/thumbnails/the-tasty-tour/regular/large.jpg'),
+      medium: require('./assets/thumbnails/the-tasty-tour/regular/medium.jpg'),
+      small: require('./assets/thumbnails/the-tasty-tour/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Undiscovered Cities',
+    regular: {
+      large: require('./assets/thumbnails/undiscovered-cities/regular/large.jpg'),
+      medium: require('./assets/thumbnails/undiscovered-cities/regular/medium.jpg'),
+      small: require('./assets/thumbnails/undiscovered-cities/regular/small.jpg'),
+    },
+    trending: {
+      large: require('./assets/thumbnails/undiscovered-cities/trending/large.jpg'),
+      small: require('./assets/thumbnails/undiscovered-cities/trending/small.jpg'),
+    },
+  },
+  {
+    name: 'Unresolved Cases',
+    regular: {
+      large: require('./assets/thumbnails/unresolved-cases/regular/large.jpg'),
+      medium: require('./assets/thumbnails/unresolved-cases/regular/medium.jpg'),
+      small: require('./assets/thumbnails/unresolved-cases/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Van Life',
+    regular: {
+      large: require('./assets/thumbnails/van-life/regular/large.jpg'),
+      medium: require('./assets/thumbnails/van-life/regular/medium.jpg'),
+      small: require('./assets/thumbnails/van-life/regular/small.jpg'),
+    },
+  },
+  {
+    name: 'Whispering Hill',
+    regular: {
+      large: require('./assets/thumbnails/whispering-hill/regular/large.jpg'),
+      medium: require('./assets/thumbnails/whispering-hill/regular/medium.jpg'),
+      small: require('./assets/thumbnails/whispering-hill/regular/small.jpg'),
+    },
+  },
+]
