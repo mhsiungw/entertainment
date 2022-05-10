49a9db09fb6b0363d08de6c31e74342b587f809b
minh_w <minhsiungw@gmail.com>
2022-05-10 11:55:25 +0800

    make GeneralLayout flex row

--- src/layouts/GeneralLayout.js
+++ src/layouts/GeneralLayout.js
@@ -1,7 +1,21 @@
 /** @jsxImportSource @emotion/react */
+import { useTheme } from '@emotion/react'
 
 const Layout = (props) => {
-  return <div css={{ display: 'flex', flexDirection: 'column', maxWidth: '768px', margin: '0 auto' }} {...props} />
+  const { useMedia } = useTheme()
+  const mq = useMedia()
+
+  const style = {
+    display: 'flex',
+    flexDirection: 'column',
+    [mq[1]]: {
+      flexDirection: 'row',
+    },
+    maxWidth: '768px',
+    margin: '0 auto',
+  }
+
+  return <div css={style} {...props} />
 }
 
 export default Layout
