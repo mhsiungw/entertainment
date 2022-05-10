82b0ed68ef48b834e9c7cd7e4815f0a740ebebe9
minh_w <minhsiungw@gmail.com>
2022-05-10 15:11:34 +0800

    add Movie.js

---
+++ src/pages/Movies.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from 'layouts/Main'
+import Recommend from '../components/Recommend'
+
+const Movies = () => {
+  return (
+    <Main>
+      <Recommend title="Movies" />
+    </Main>
+  )
+}
+
+export default Movies
