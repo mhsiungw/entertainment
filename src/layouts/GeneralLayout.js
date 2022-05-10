d0649640f8fa99c54f95a93324884edc5c065a50
minh_w <minhsiungw@gmail.com>
2022-05-10 11:20:53 +0800

    modify GeneralLayout

--- src/layouts/GeneralLayout.js
+++ src/layouts/GeneralLayout.js
@@ -1,8 +1,7 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
 
 const Layout = (props) => {
-  return <div css={{ display: 'flex', flexDirection: 'column' }} {...props} />
+  return <div css={{ display: 'flex', flexDirection: 'column', maxWidth: '768px', margin: '0 auto' }} {...props} />
 }
 
 export default Layout
