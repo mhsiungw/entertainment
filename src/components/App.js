1d173cf5dca381561577b900955d0753b33ec4a5
minh_w <minhsiungw@gmail.com>
2022-05-12 13:42:31 +0800

    trimming

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
-/** @jsxImportSource @emotion/react */
-import { useTheme } from '@emotion/react'
 import Layout from 'layouts/GeneralLayout'
 import Header from '../layouts/Header'
 import { Outlet } from 'react-router-dom'
 
-function App(props) {
-  const { colors, useMedia } = useTheme()
-  const mq = useMedia()
-
+function App() {
   return (
     <Layout>
       <Header />
